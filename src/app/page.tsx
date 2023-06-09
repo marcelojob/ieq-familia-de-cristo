254accba03d167e0f8d1747fe50fcd9edd3fd239
marcelojob <marcelojob1995@gmail.com>
2023-06-09 17:47:14 -0300

    fix: adjust import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { MapPinIcon, UserIcon } from '@heroicons/react/24/outline';
 import { useCallback, useEffect, useState } from 'react';
-import { findCells } from '#/api/route';
+import { findCells } from '#/api';
 import { cellProps } from '#/api/models';
 import Loader from '#/components/Loader';
 import { roleLabels, weekDayLabels } from '#/utils/labels';
