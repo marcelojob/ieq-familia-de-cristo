9a3d279e1b079e2e4f68566c7e5abb85caa80bf1
marcelojob <marcelojob1995@gmail.com>
2023-06-10 16:27:18 -0300

    feat: added page hero banner

--- src/app/programacao/page.tsx
+++ src/app/programacao/page.tsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { findSchedule } from '#/api';
 import { ScheduleProps } from '#/api/models';
 import Loader from '#/components/Loader';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 const Schedule: React.FunctionComponent = () => {
   
@@ -31,9 +32,20 @@ const Schedule: React.FunctionComponent = () => {
 
   return (
 
-    <main className="flex flex-col items-center justify-between lg:py-10 p-5 bg-black">
+    <main className="flex flex-col items-center justify-between lg:pb-10 pt-0 pt-5 pb-5 bg-black">
       <Loader show={isLoading} />
-      <h1>{schedule?.title}</h1>
+      <div className="bg-cover bg-center h-auto text-white py-24 px-10 object-fill w-full bg-black bg-opacity-75 bg-blend-multiply" style={{backgroundImage: `url("${schedule?.theme.image}")`}}>
+        <div className="md:w-1/2">
+          <p className="font-bold text-sm uppercase">Tema do mês</p>
+          <p className="text-4xl font-bold">{schedule?.theme.title}</p>
+          <p className="text-lg mb-2 mt-5 leading-normal">{schedule?.theme.text}</p>
+          <p className="font-bold text-sm">{schedule?.theme.reference}</p>
+        </div>  
+      </div>
+      <h2 className=" text-center mb-3 mt-5 text-2xl font-bold tracking-tight text-gray-300 uppercase">
+        {schedule?.title}
+      </h2>
+      <ChevronDownIcon className="h-6 w-6 text-white mb-3 mx-auto" />
       {schedule?.schedule.map( (event, index) => {
         return (
           <div key={index}> 
